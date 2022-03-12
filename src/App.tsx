39904ff2723e6a267e8879a9e53ed80b7661f47b
Matthew Charlton <mattycharlton101@gmail.com>
2022-03-12 22:14:18 +0000

    Adds App with basic chess

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { Container } from './App.styled'
+import { Chess, ChessInstance, Square } from 'chess.js'
+import { Chessboard } from 'react-chessboard'
+
+const App: React.FC = () => {
+  const [game, setGame] = useState(new Chess())
+
+  const safeGameMutate = (modify: Function) => {
+    setGame((g) => {
+      const update = { ...g }
+      modify(update)
+      return update
+    })
+  }
+
+  const makeRandomMove = () => {
+    const possibleMoves = game.moves()
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return // exit if the game is over
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length)
+    safeGameMutate((game: ChessInstance) => {
+      game.move(possibleMoves[randomIndex])
+    })
+  }
+
+  const onDrop = (sourceSquare: Square, targetSquare: Square) => {
+    let move = null
+    safeGameMutate((game: ChessInstance) => {
+      move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: 'q', // always promote to a queen for example simplicity
+      })
+    })
+    if (move === null) return false // illegal move
+    setTimeout(makeRandomMove, 200)
+    return true
+  }
+
+  return (
+    <Container>
+      <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+    </Container>
+  )
+}
+
+export default App
